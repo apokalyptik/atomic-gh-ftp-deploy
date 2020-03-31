930b579f32d7a8f87532e5e5b8a13f4bbb112c80
Demitrious Kelly <apokalyptik@apokalyptik.com>
2020-03-31 10:12:03 -0700

    header

--- __wp__/index.php
+++ __wp__/index.php
@@ -1,4 +1,4 @@
 <?php
 
-echo 'Hello <b>' . htmlentities( $_SERVER['X_FORWARDED_FOR'] ) . '</b> the current time is: ' . microtime( true );
+echo 'Hello <b>' . htmlentities( $_SERVER['HTTP_X_FORWARDED_FOR'] ) . '</b> the current time is: ' . microtime( true );
 
