f4a79848a191a70f573f0d8cf9eeb01f8098e9cf
apokalyptik <apokalyptik@apokalyptik.com>
2023-07-21 11:44:25 -0700

    Update custom-redirects.php

--- custom-redirects.php
+++ custom-redirects.php
@@ -1,2 +1,8 @@
 <?php
 
+echo sprintf(
+	"%s Deployed on<br/>%s<br/>%d seconds ago",
+	htmlentities( trim( file_get_contents( '.git-ftp.log' ) ) ),
+	date( 'r', filemtime( '.git-ftp.log' ) ),
+	time() - filemtime( '.git-ftp.log' )
+);
